7d006697d8d01193cc7b3dec23284fd582b25bbe
jlbuild <juliabuildbot@gmail.com>
2020-07-27 15:12:26 +0000

    mlpack_jll build 3.3.2+1

--- src/wrappers/i686-w64-mingw32-cxx03.jl
+++ src/wrappers/i686-w64-mingw32-cxx03.jl
@@ -667,8 +667,6 @@ function __init__()
     foreach(p -> append!(PATH_list, p), (boost_jll.PATH_list, armadillo_jll.PATH_list, OpenBLAS_jll.PATH_list,))
     foreach(p -> append!(LIBPATH_list, p), (boost_jll.LIBPATH_list, armadillo_jll.LIBPATH_list, OpenBLAS_jll.LIBPATH_list,))
 
-    # Lastly, we need to add to LIBPATH_list the libraries provided by Julia
-    append!(LIBPATH_list, [Sys.BINDIR, joinpath(Sys.BINDIR, Base.LIBDIR, "julia"), joinpath(Sys.BINDIR, Base.LIBDIR)])
     global libmlpack_path = normpath(joinpath(artifact_dir, libmlpack_splitpath...))
 
     # Manually `dlopen()` this right now so that future invocations
@@ -995,12 +993,8 @@ function __init__()
     filter!(!isempty, unique!(PATH_list))
     filter!(!isempty, unique!(LIBPATH_list))
     global PATH = join(PATH_list, ';')
-    global LIBPATH = join(LIBPATH_list, ';')
+    global LIBPATH = join(vcat(LIBPATH_list, [Sys.BINDIR, joinpath(Sys.BINDIR, Base.LIBDIR, "julia"), joinpath(Sys.BINDIR, Base.LIBDIR)]), ';')
 
-    # Add each element of LIBPATH to our DL_LOAD_PATH (necessary on platforms
-    # that don't honor our "already opened" trick)
-    #for lp in LIBPATH_list
-    #    push!(DL_LOAD_PATH, lp)
-    #end
+    
 end  # __init__()
 
